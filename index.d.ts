e2a9d9fb572d7f82fd99296f081fd82e86c1c2cb
Sefinek <contact@sefinek.net>
2023-10-21 04:21:43 +0200

    Fix types

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,24 @@
-export function encode(text: string): string;
-export function decode(text: string): string;
-export const version: string;
+declare module '@sefinek/rot47' {
+    /**
+     * Encodes a string in ROT47.
+     *
+     * @param input - The input string to be encoded.
+     * @returns The ROT47-encoded string.
+     */
+    function encode(input: string): string;
+
+    /**
+     * Decodes a string from ROT47.
+     *
+     * @param input - The input string to be decoded.
+     * @returns The ROT47-decoded string.
+     */
+    function decode(input: string): string;
+
+    /**
+     * The version of the @sefinek/rot47 module.
+     */
+    const version: string;
+
+    export { encode, decode, version };
+}
